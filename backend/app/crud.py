86a5ca8b69d41bbb525106263c35d2771f665f7a
Arescoreadmin <admin@arescore.ai>
2025-08-06 12:52:55 -0400

    Fix CRUD functions to support user creation and pagination

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,14 +1,17 @@
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 
-def get_users(db: Session):
-    return db.query(models.User).all()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-def create_user(db: Session, email: str, hashed_password: str):
-    db_user = models.User(email=email, hashed_password=hashed_password)
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(email=user.email, hashed_password=user.hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
